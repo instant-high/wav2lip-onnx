49c84be7585961d545754f750819dd4d14590e8c
Thomas De <heinzelmann1963@gmail.com>
2024-10-14 00:51:39 +0200

    Update inference_onnxModel.py
    changed face detection det_thresh 0.6 to 0.3

--- inference_onnxModel.py
+++ inference_onnxModel.py
@@ -76,7 +76,7 @@ def get_smoothened_boxes(boxes, T):
 def face_detect(images):
 
 	detector = Face_detect_crop(name='antelope', root='./insightface_func/models')
-	detector.prepare(ctx_id= 0, det_thresh=0.6, det_size=(320,320),mode='none')
+	detector.prepare(ctx_id= 0, det_thresh=0.3, det_size=(320,320),mode='none')
 
 	predictions = []
 	for i in tqdm(range(0, len(images))):
